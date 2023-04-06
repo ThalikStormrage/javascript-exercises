bf1498f6aee385ceade1b9a498e1f65f3691ea76
ThalikStormrage <cwraiz@icloud.com>
2023-04-05 20:23:05 -0500

    Created repeatString Function, Passed all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let repeatedString = '';
+    if (num < 0) {
+        return repeatedString = 'ERROR';
+    }
+    while (num > 0) {
+        repeatedString += string;
+        num--;
+    };
+    return repeatedString;
+}
 
-};
-
+repeatString('hey', 1);
 // Do not edit below this line
 module.exports = repeatString;
