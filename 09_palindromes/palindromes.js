731982e976f3c18f00232503ef971e42e8fb851c
ThalikStormrage <cwraiz@icloud.com>
2023-04-08 05:20:35 -0500

    Created palindromes function, passed all tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
-
+const palindromes = function (words) {
+    const lowerCaseWords = words.toLowerCase()
+    const noSpacesWords = lowerCaseWords.replaceAll(' ', '');
+    const splitWords = noSpacesWords.split('');
+    const punctuation = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
+    const noPunctuation = splitWords.filter(function(letter) {
+        return punctuation.indexOf(letter) === -1;
+    });
+    const plainWords = noPunctuation.join();
+    const reversedWords = noPunctuation.reverse();
+    const joinedWords = reversedWords.join();
+    
+    if (joinedWords === plainWords){
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
