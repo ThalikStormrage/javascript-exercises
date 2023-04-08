6b3d375ac0cdd4055d6bb0cf985b1f2151e0f35d
ThalikStormrage <cwraiz@icloud.com>
2023-04-08 02:28:19 -0500

    Created sumAll function, passed all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,15 @@
-const sumAll = function() {
-
-};
-
-// Do not edit below this line
+const sumAll = function(firstNum, secondNum) {
+  if ((firstNum || secondNum) < 0 || typeof (firstNum && secondNum) !== "number"){
+    return 'ERROR'
+  }
+  let array = [];
+  const startingNum = Math.min(firstNum, secondNum);
+  const endingNum = Math.max(firstNum, secondNum);
+    for (i = startingNum; i <= endingNum; i++){
+        array.push(i);
+    }
+  const sum = array.reduce((arraySum, currentArrayValue) => 
+    arraySum + currentArrayValue, 0);
+  return sum;
+}
 module.exports = sumAll;
