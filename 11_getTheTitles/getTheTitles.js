ac781de6ee1852d6105f66d2f65dbfff16d2f644
ThalikStormrage <cwraiz@icloud.com>
2023-04-08 06:58:46 -0500

    Created getTheTitles function, passed all tests

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    let titles = array.map(({title}) => title);
+    console.log(titles);
+    return titles;
 };
 
 // Do not edit below this line
