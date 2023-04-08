60e2fcb86faefa41cb214a6e910a7d436eb3a764
ThalikStormrage <cwraiz@icloud.com>
2023-04-08 04:48:06 -0500

    Created add, subtract, sum, multiply, power, and factorial functions, passed all tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,38 @@
-const add = function() {
-	
+const add = function(firstNum, secondNum) {
+  const result = firstNum + secondNum;
+	return result;
 };
 
-const subtract = function() {
-	
+const subtract = function(firstNum, secondNum) {
+	const result = firstNum - secondNum;
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	const result = array.reduce((total, nextNumber) => total + nextNumber, 0);
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+	const result = array.reduce((total, nextNumber) => total * nextNumber, 1);
+  return result;
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+  const result = num ** power;
+  return result;
 };
 
-const factorial = function() {
-	
-};
+const factorial = function(num) {
+  let result = 1;
+  if (num == 0 || num == 1){
+    return result;
+  } else {
+    for (i = num; i >= 1; i--)
+      result = result * i;
+  }
+  return result;
+  };
 
 // Do not edit below this line
 module.exports = {
