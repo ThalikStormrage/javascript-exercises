bc3d6f638d23a88b20471566e39129789d9464be
ThalikStormrage <cwraiz@icloud.com>
2023-04-08 06:37:41 -0500

    Created fibonacci functions, passed all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,24 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let iterations = parseInt(num, 10);
+    let n1 = 0;
+    let n2 = 1;
+    let nextIteration;
+    let i = 1;
+    if (iterations > 0) {
+        do {
+            nextIteration = n1 + n2;
+            n1 = n2;
+            n2 = nextIteration;
+            i++;
+        } while (i < iterations);
+        return nextIteration;
+    } else {
+        return 'OOPS';
+    }
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+//var text = '42px';
+//var integer = parseInt(text, 10);
