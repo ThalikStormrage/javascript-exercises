7b079ce10db434b4b3a24efc271662e35ed5d865
ThalikStormrage <cwraiz@icloud.com>
2023-04-08 00:07:37 -0500

    Created removeFromArray Function, Passed all Tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(array, ...remove) {
+    const removeArray = Array.from(remove);
+     for (i = 0; i < removeArray.length; i++) {
+      let removeElement = removeArray[i];
+      const index = array.indexOf(removeElement);
+      if (index > -1) {
+        array.splice(index, 1);
+      }
+     }
+    return array;
+   }
 
 // Do not edit below this line
 module.exports = removeFromArray;
